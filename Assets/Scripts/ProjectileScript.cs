aaf7ded3f20e4cadf127741b30636f783323ad06
Adina Pandelea <adina.pandelea@axsys.ro>
2020-04-26 20:17:21 +0300

    add attack-ability

---
+++ Assets/Scripts/ProjectileScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Se stabileste durata de viata a proiectilului
+        Destroy(this.gameObject, 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //Daca proiectilul loveste ceva acesta se ditruge
+    //Daca proiectulul loveste un inamic ii aplica daune
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            collision.transform.SendMessage("UpdateHealth", 15);
+            Destroy(this.gameObject);
+        }
+        else
+        if (collision.gameObject.tag != "Player")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
