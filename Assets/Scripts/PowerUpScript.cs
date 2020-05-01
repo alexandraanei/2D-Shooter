71b8622f70edb484b3849b9fde389301db53d4a8
alex staniea <alex.staniea98@gmail.com>
2020-05-01 12:20:53 +0300

    powerup

---
+++ Assets/Scripts/PowerUpScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpScript : MonoBehaviour
+{
+
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Cand playerul atinge power-up ul cu inima i se da 50 viata si se distruge acest obiect din scena
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            player.GetComponent<CC>().DamageTaken(-50);
+            Destroy(this.gameObject);
+        }
+    }
+}
